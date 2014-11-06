d7c70300ec48292250c502c0c3cae02c94fb270d
Ricardo <ricardo.silva@valid.com.br>
2014-11-06 10:49:43 -0200

    Added tip to handle exception from custom control

---
+++ CustomControlExceptionHandle/CustomControlExceptionHandle/CustomTextBox.cs
@@ -0,0 +1,46 @@
+﻿namespace CustomControlExceptionHandle
+{
+    using System;
+    using System.Windows.Forms;
+
+    public delegate void ExceptionDelegate(Exception ex, object sender);
+
+    public class CustomTextBox : TextBox
+    {
+        public CustomTextBox()
+        {
+            this.TextChanged += new EventHandler(CustomTextBox_TextChanged);
+        }
+
+        /// <summary>
+        /// Gets or sets the delegate to when some exception will be thrown.
+        /// This will called anytime this control throw some exception.
+        /// </summary>
+        public ExceptionDelegate OnExceptionThrown { get; set; }
+
+        void CustomTextBox_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                // .. do something.
+            }
+            catch (Exception ex)
+            {
+                // .. something unexpected happened. Let's throw this error
+                // outside this component. Let who uses this handle with the error.
+                if (this.OnExceptionThrown != null)
+                {
+                    //Now we're dispatching some kind of event, indicating that some exception was thrown.
+                    //Let who uses this component handle with exception.
+                    this.OnExceptionThrown.Invoke(ex, sender); 
+                }
+                else
+                {
+                    throw ex; //This will throw as unhandled exception, but is better do this
+                              //than override exception with NullArgumentException if delegate is null.
+                              //You can guarantee by constructor too that the delegate has some value assigned to it.
+                }
+            }
+        }
+    }
+}
---
+++ CustomControlExceptionHandle/CustomControlExceptionHandle/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CustomControlExceptionHandle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            var customTextbox = new CustomTextBox();
+
+            //We assigned one method with same signature of delegate created inside
+            //custom text box. Everytime custom text box call this delegate, the method
+            //that we assigned that will be called. Inside this we can handle with the
+            //exception.
+            customTextbox.OnExceptionThrown += this.SomeExceptionHappened;
+        }
+
+        public void SomeExceptionHappened(Exception ex, object sender)
+        {
+            //... handle exception. For example:
+            MessageBox.Show(
+                "Ops! Some error happened \n " + ex.Message,
+                "E R R O R",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Error);
+        }
+    }
+}
